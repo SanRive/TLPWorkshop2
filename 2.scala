77fac70e3335729492bdd0670c7477d99a7210b0
SanRive <sriverame@unal.edu.co>
2025-02-12 22:40:43 -0500

    test cases for 2nd exercise

--- 2.scala
+++ 2.scala
@@ -1,6 +1,14 @@
-val frases = List("Scala es genial", "Programar es divertido", "La palabra más larga")
-val palabraMasLarga = frases
-  .flatMap(_.split(" ")) // Dividir cada frase en palabras
-  .reduceLeft((a, b) => if (a.length > b.length) a else b) // Reducir seleccionando la palabra más larga
+object PalabraMasExtensa {
 
-println(s"La palabra más larga es: $palabraMasLarga")2.scala
+  // Función para encontrar la palabra más extensa en una lista de frases utilizando reduceLeft
+  def palabraMasExtensa(frases: List[String]): String = {
+    frases.flatMap(_.split(" ")).reduceLeft((a, b) => if (a.length >= b.length) a else b)
+  }
+
+  // Ejecución de ejemplo
+  def main(args: Array[String]): Unit = {
+    val frases = List("Esta es una prueba", "Buscando la palabra más extensa", "Scala es poderoso")
+    println("Palabra más extensa:")
+    println(palabraMasExtensa(frases))
+  }
+}
