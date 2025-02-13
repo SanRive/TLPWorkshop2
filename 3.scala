91883446655ddb5d5e70adb862c06803faf61cef
SanRive <sriverame@unal.edu.co>
2025-02-12 22:41:42 -0500

    test cases for 3rd exercise

--- 3.scala
+++ 3.scala
@@ -1,6 +1,15 @@
-val numeros = List(1, 2, 3, 4, 5, 6, 7, 8)
-val sumaCuadradosPares = numeros
-  .filter(_ % 2 == 0) // Filtrar los números pares
-  .fold(0)((acc, n) => acc + n * n) // Calcular la suma de los cuadrados
+object SumaCuadradosPares {
 
-println(s"La suma de los cuadrados de los números pares es: $sumaCuadradosPares")
+  // Función para calcular la suma de los cuadrados de los números pares
+  def sumaCuadradosPares(lista: List[Int]): Int = {
+    lista
+      .filter(_ % 2 == 0) // Filtrar los números pares
+      .foldLeft(0)((acc, n) => acc + n * n) // Sumar los cuadrados
+  }
+  // Ejecución de ejemplo
+  def main(args: Array[String]): Unit = {
+    val numeros = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    println("Suma de los cuadrados de los números pares:")
+    println(sumaCuadradosPares(numeros))
+  }
+}
