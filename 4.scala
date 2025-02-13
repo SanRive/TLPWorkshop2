719bf736e9464b2794693a61538e29df07092b6c
SanRive <sriverame@unal.edu.co>
2025-02-12 22:55:15 -0500

    test cases for 4th exercise

--- 4.scala
+++ 4.scala
@@ -1,18 +1,18 @@
-def distanciaOptima(
-                     puntos: List[(Double, Double)],
-                     distancia: ((Double, Double), (Double, Double)) => Double
-                   ): Double = {
-  puntos.combinations(2) // Generar todas las combinaciones de pares de puntos
-    .map { case List(p1, p2) => distancia(p1, p2) } // Calcular la distancia para cada par
-    .min // Encontrar la distancia mínima
+object DistanciaOptima {
+
+  // Función de orden superior para calcular la distancia mínima entre una lista de puntos
+  def distanciaOptima(puntos: List[(Double, Double)], distancia: ((Double, Double), (Double, Double)) => Double): Double = {
+    if (puntos.length < 2) 0.0
+    else puntos.combinations(2).map(pair => distancia(pair(0), pair(1))).min
+  }
+
+  // Función para calcular la distancia euclidiana entre dos puntos
+  def distanciaEuclidiana(p1: (Double, Double), p2: (Double, Double)): Double = {
+    math.sqrt(math.pow(p1._1 - p2._1, 2) + math.pow(p1._2 - p2._2, 2))
+  }
+
+  def main(args: Array[String]): Unit = {
+    val puntos = List((1.0, 2.0), (3.0, 4.0), (5.0, 6.0), (7.0, 8.0))
+    println(s"La distancia mínima entre los puntos es: ${distanciaOptima(puntos, distanciaEuclidiana)}")
+  }
 }
-
-// Función para calcular la distancia Euclidiana entre dos puntos
-val distanciaEuclidiana: ((Double, Double), (Double, Double)) => Double = {
-  case ((x1, y1), (x2, y2)) => Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
-}
-
-val puntos = List((1.0, 2.0), (4.0, 6.0), (7.0, 8.0))
-val distanciaMinima = distanciaOptima(puntos, distanciaEuclidiana)
-
-println(s"La distancia mínima entre los puntos es: $distanciaMinima")
